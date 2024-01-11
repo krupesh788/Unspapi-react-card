d504ceb50cc247e64b2aeeb82042159f3dd7f1b6
Ravibhola0126 <bhola.ravi26@gmail.com>
2024-01-11 15:54:37 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [value, setValue] = useState("");
+  const [results, setResults] = useState([]);
+
+  const fetchImages = () => {
+fetch(
+      `https://api.unsplash.com/search/photos?client_id=mtmVjlZVowBLDttrAKdnfNw3HbJ4AhFHfGufiNWaYKM&query=${value}&orientation=squarish&username&html`
+    )
+
+      .then((res) => res.json())
+
+      .then((data) => {
+        console.log(data);
+        setResults(data.results);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <div className="myDiv">
+          <span className="heading">Search</span>
+
+
+          <input 
+            style={{ width: "100%" }}
+            type="text"
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+          />
+
+          <br/>
+          <br/>
+
+          <button 
+            className="btn btn-primary"
+            onClick={() => fetchImages()}
+          >
+            Send
+          </button>
+          
+        
+        </div>
+
+
+
+        <div className="container mt-3">
+          <div className="row mb-5">
+          
+              {results.map((item) => {
+                return (
+                  
+                  <div className="col-md-4 mb-5">
+                    <div className="card">
+                    <img 
+                      className="card-img-top"
+                      alt="image not found"
+                      key={item.id}
+                      src={item.urls.regular}
+                      
+                      />
+
+                    <div className="card-body">
+
+                    <h5 className="card-title user-1">
+                      Photographer:  {item.user.username}
+                    </h5>
+
+                    <h5 className="card-text user-2">
+                      Username: {item.user.username}
+                    </h5>
+
+                    <h5 className="card-text user-3">
+                      User Profile:  <a href="..." className="link-1">
+                        {item.user.links.html}
+                        </a> 
+                      
+                    </h5>
+                    
+                  </div>
+                  </div>
+                    </div>
+                  
+                );
+              })}
+          
+              </div>
+              </div>
+        
+          </div>
+    
+    </>
   );
 }
 
